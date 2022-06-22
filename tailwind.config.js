97a024bc58c0dc783e374b3af4c9feb31a9d3d5d
innocentdavid <paulinnocent04@gmail.com>
2022-06-22 22:31:45 +0100

    dated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,3 @@
-// module.exports = {
-//   darkMode: 'class',
-//   purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
-//   content: [
-//     './pages/**/*.{js,ts,jsx,tsx}',
-//     './components/**/*.{js,ts,jsx,tsx}',
-//   ],
-//   theme: {
-//     typography: (theme) => ({
-//       dark: {
-//         css: {
-//           color: 'white',
-//         },
-//       },
-//     }),
-//     extend: {
-//       colors: {
-//         primary: "var(--primary)",
-//         secondary: "var(--secondary)",
-//         main: "var(--main)",
-//         baseColor: '#EB004E',
-//         background: "var(--background)",
-//         header: "var(--header)",
-//         accent: "var(--accent)",
-//       },
-//     },
-//   },
-//   variants: {},
-//   plugins: [require('@tailwindcss/typography')],
-// }
-
-
-
-
-
 module.exports = {
   darkMode: "class",
   purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
